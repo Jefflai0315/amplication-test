84a04a1e9440d5edea7f44cf4621187a07e39fe0
Jefflai0315 <79297365+Jefflai0315@users.noreply.github.com>
2022-02-11 14:42:48 +0800

    amplication-build-ckzi1lvhg115544701s69k3ro7d3

---
+++ admin-ui/src/api/project/ProjectCreateInput.ts
@@ -0,0 +1,5 @@
+export type ProjectCreateInput = {
+  description?: string | null;
+  name?: string | null;
+  startDate?: Date | null;
+};
---
+++ admin-ui/src/api/project/ProjectUpdateInput.ts
@@ -0,0 +1,5 @@
+export type ProjectUpdateInput = {
+  description?: string | null;
+  name?: string | null;
+  startDate?: Date | null;
+};
